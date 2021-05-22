b22a06638f4e20842fcc9e67053880be61b6c715
FoxGreedy <hotmus8500@gmail.com>
2021-05-22 14:52:17 -0300

    feat(hr-worker): create worker route

---
+++ hr-worker/src/main/java/com/example/demo/resources/WorkerResources.java
@@ -0,0 +1,33 @@
+package com.example.demo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.Worker;
+import com.example.demo.repository.WorkerRepository;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResources {
+
+	@Autowired
+	private WorkerRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Worker>> findAll(){
+		List<Worker> list = repository.findAll();
+		return ResponseEntity.ok(list);
+	} 
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		Worker obj = repository.findById(id).get();
+		return ResponseEntity.ok(obj);
+	} 
+}
