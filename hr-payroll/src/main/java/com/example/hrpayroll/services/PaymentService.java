ec847222dd5a81332e96c51e814e6fcce6006ac1
FoxGreedy <hotmus8500@gmail.com>
2021-05-25 23:33:08 -0300

    feat(hr-payroll): request service worker

--- hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
@@ -1,13 +1,30 @@
 package com.example.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.hrpayroll.entities.Payment;
+import com.example.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 100.00, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", "" + workerId);
+
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
