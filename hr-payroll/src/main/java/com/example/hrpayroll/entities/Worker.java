93d786e6f84e8ff5865f0e07e0aaa70096472ac7
FoxGreedy <hotmus8500@gmail.com>
2021-05-26 14:41:11 -0300

    feat(hr-eureka-server): created

--- hr-payroll/src/main/java/com/example/hrpayroll/entities/Worker.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/entities/Worker.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 
 public class Worker implements Serializable {
-
+	private static final long serialVersionUID = 1L;
 	private Long id;
 	private String name;
 	private Double dailyIncome;
