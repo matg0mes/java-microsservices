ba40b17c5a9d984212c395b4e3a6de6e8ea919a7
FoxGreedy <hotmus8500@gmail.com>
2021-05-30 16:53:24 -0300

    feat(payment): create fallback service worker

--- hr-worker/src/main/java/com/example/demo/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/example/demo/resources/WorkerResource.java
@@ -36,9 +36,6 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
-		
-		logger.info("PORT" + env.getProperty("local.server.port"));
-		
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	} 
