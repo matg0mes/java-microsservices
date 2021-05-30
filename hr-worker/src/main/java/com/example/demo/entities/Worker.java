075eeb4e05573812f658684418ed049f4614a651
FoxGreedy <hotmus8500@gmail.com>
2021-05-30 15:26:53 -0300

    feat(eureka): configuring eureka clients

--- hr-worker/src/main/java/com/example/demo/entities/Worker.java
+++ hr-worker/src/main/java/com/example/demo/entities/Worker.java
@@ -11,6 +11,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_worker")
 public class Worker implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
