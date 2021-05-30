7a56f8e77807a79ebf148186cee999cf8f6155a2
FoxGreedy <hotmus8500@gmail.com>
2021-05-30 20:17:14 -0300

    feat(worker): add actuator

--- hr-worker/src/main/java/com/example/demo/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/example/demo/HrWorkerApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
--- hr-worker/src/main/java/com/example/demo/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/example/demo/resources/WorkerResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entities.Worker;
 import com.example.demo.repository.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
