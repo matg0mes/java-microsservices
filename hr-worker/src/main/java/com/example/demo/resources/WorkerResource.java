4f9d2f20853f27ed214ba15e9a829ce42fb2642a
FoxGreedy <hotmus8500@gmail.com>
2021-05-30 19:21:29 -0300

    feat(worker): create implement worker

--- hr-worker/src/main/java/com/example/demo/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/example/demo/resources/WorkerResource.java
@@ -3,6 +3,7 @@ package com.example.demo.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,22 +13,31 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entities.Worker;
 import com.example.demo.repository.WorkerRepository;
 
-
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private WorkerRepository repository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Worker>> findAll(){
+	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
-	} 
-	
+	}
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Worker> findById(@PathVariable Long id){
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
-	} 
+	}
+
+	@GetMapping(value = "/configs")
+	public ResponseEntity<String> getConfigs(){
+		return ResponseEntity.ok(testConfig);
+	}
+
 }
