f3fec9239fca135817c16d2e90d21068b505dbf4
FoxGreedy <hotmus8500@gmail.com>
2021-05-30 17:19:06 -0300

    feat(gateway): configure api gateway

--- hr-api-gateway-zuul/src/main/java/com/example/demo/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/example/demo/HrApiGatewayZuulApplication.java
@@ -2,7 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
